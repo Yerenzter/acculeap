d649d37d94548880d2151a044a78ceb54c42d92d
Yerenzter <73117946+Yerenzter@users.noreply.github.com>
2021-10-22 02:04:13 +0800

    0.0.11

--- js/acculeap.js
+++ js/acculeap.js
@@ -2,5 +2,6 @@ OnResult = () => {
     getYear = document.getElementById("yr-text").value;
     prevLeapYear = getYear - 4;
     nextLeapYear = getYear + 4;
-    document.getElementById("yr-result").innerHTML = getYear == '' ? '...' : getYear % 4 == 0 && getYear % 100 != 0 || getYear % 400 == 0 ? getYear + " is a leap year." + " previous leap year:" + prevLeapYear + " next leap year:" + nextLeapYear : getYear + " is not a leap year.";
+    document.getElementById("yr-result").innerHTML = getYear == '' ? '...' : getYear % 4 == 0 && getYear % 100 != 0 || getYear % 400 == 0 ? getYear + " is a leap year." : getYear + " is not a leap year.";
+    document.getElementById("yr-result1").innerHTML = " previous leap year:" + prevLeapYear + " next leap year:" + nextLeapYear;
 }
